cfa1a9f4a0ae94d2da5a8605e7a33cfcd746ce86
vivekchandel8 <vivekchandel8@gmail.com>
2023-04-17 19:25:26 +0530

    implementing Parallax-demo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
+  const [offset, setOffset] = useState(0);
+  useEffect(() => {
+    function handleScroll() {
+      setOffset(window.pageYOffset);
+    }
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [offset]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <section
+        className="hero"
+        style={{ transform: `translateY(${offset * 0.5}px)` }}
+      >
+        <div className="parallax"></div>
+      </section>
+      <section className="second-layer"></section>
+      <section className="third-layer"></section>
     </div>
   );
 }
